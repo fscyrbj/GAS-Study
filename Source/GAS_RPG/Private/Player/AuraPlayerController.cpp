aafa8948d4dce5b1c12b3d19b1ec51ea6c3d463b
t659792935@163.com <t659792935@163.com>
2025-04-23 10:33:11 +0800

    基础设置

---
+++ Source/GAS_RPG/Private/Player/AuraPlayerController.cpp
@@ -0,0 +1,114 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Player/AuraPlayerController.h"
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
+#include "Interactiom/EnemyInterface.h"
+
+AAuraPlayerController::AAuraPlayerController()
+{
+	bReplicates = true;
+}
+
+void AAuraPlayerController::PlayerTick(float DeltaTime)
+{
+	Super::PlayerTick(DeltaTime);
+	CursorTrace();
+	
+}
+void AAuraPlayerController::CursorTrace()
+{
+	FHitResult CursorHit;
+	GetHitResultUnderCursor(ECC_Visibility, false, CursorHit);
+	if (!CursorHit.bBlockingHit) return;
+
+	LastActor = ThisActor;
+	ThisActor = Cast<IEnemyInterface>(CursorHit.GetActor());
+
+	/** 
+	 * A.LastActor == nullptr, ThisActor == nullptr, do nothing;
+	 * B.LastActor == nullptr, ThisActor != nullptr, Highlight ThisActor;
+	 * C.LastActor != nullptr, ThisActor == nullptr, UnHighlight LastActor;
+	 * D.LastActor != nullptr, ThisActor != nullptr, LastActor = ThisActor, do nothing;
+	 * E.LastActor != nullptr, ThisAcotr != nullptr, LastActor != ThisActor, UnHighlight LastActor, Hightlight ThisActor;
+	 */
+	if (LastActor == nullptr)
+	{
+		if (ThisActor != nullptr)
+		{
+			//Case B
+			ThisActor->HighlightActor();
+		}
+		else
+		{
+			//Case A
+		}
+	}
+	else
+	{
+		if (ThisActor == nullptr)
+		{
+			//Case C
+			LastActor->UnHighlightActor();
+		}
+		else
+		{
+			if (ThisActor != LastActor)
+			{
+				//Case E
+				ThisActor->HighlightActor();
+				LastActor->UnHighlightActor();
+			}
+			else
+			{
+				//Case D
+			}
+		}
+	}
+}
+
+void AAuraPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	check(AuraContext);
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	check(Subsystem);
+	Subsystem->AddMappingContext(AuraContext, 0);
+
+	bShowMouseCursor = true;
+	DefaultMouseCursor = EMouseCursor::Default;
+
+	FInputModeGameAndUI InputModeDate;
+	InputModeDate.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+	InputModeDate.SetHideCursorDuringCapture(false);
+	SetInputMode(InputModeDate);
+	
+
+}
+
+void AAuraPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+	UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(InputComponent);
+	EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &AAuraPlayerController::Move);
+}
+
+void AAuraPlayerController::Move(const FInputActionValue& InputActionValue) 
+{
+	const FVector2D InputAxisVector = InputActionValue.Get<FVector2D>();
+	const FRotator Rotation = GetControlRotation();
+	const FRotator YawRotation(0.f,Rotation.Yaw, 0.0f);
+
+	const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+	const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+
+	if (APawn* ControlledPawn = GetPawn<APawn>())
+	{
+		ControlledPawn->AddMovementInput(ForwardDirection, InputAxisVector.Y);
+		ControlledPawn->AddMovementInput(RightDirection, InputAxisVector.X);
+		
+	}
+	
+}
---
+++ Source/GAS_RPG/Public/Player/AuraPlayerState.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "AuraPlayerState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAS_RPG_API AAuraPlayerState : public APlayerState
+{
+	GENERATED_BODY()
+	
+};
